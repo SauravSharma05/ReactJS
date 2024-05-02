5cd619ea56ed6ce83449cdf01905913f513bf076
Saurav Sharma <saurav5122000@gmail.com>
2024-05-02 20:32:43 +0530

    prac updation

---
+++ practice/bookingtry/src/Header1.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header1() {
+  return (
+    <div className="App">
+
+        <div className='Header1 df'>
+
+        <div className='Headera df aic'>Booking.com</div>
+        <div className='Headerb df'>
+            <div className='Headerb1 df aic'>India</div>
+            <div className='Headerb1 df aic'>flag</div>
+            <div className='Headerb1 df aic'>help icon</div>
+            <div className='Headerb1 df aic'>List your property</div>
+            <div className='Headerb1 btn df aic'><button>Register</button></div>
+            <div className='Headerb1 btn df aic'><button>Sign in</button></div>
+        </div>
+            
+        </div>
+
+    </div>
+  );
+}
+
+export default Header1;
--- practice/hello123/src/App.js
+++ practice/hello123/src/App.js
@@ -1,10 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import './try.css';
 
 function App() {
+  fetch('https://jsonplaceholder.typicode.com/photos')
+  .then((response)=>{return response.json()})
+  .then((data)=>{
+//     let test = ` <table border="1">
+//     <thead>
+//         <tr>
+//             <th>Id</th>
+//             <th>Title</th>
+//             <th>URL</th>
+//             <th>THUMBNAILURL</th>
+//         </tr>
+//     </thead>
+//     <tbody>`;
+//     data.map((v)=>{
+//         console.log(v.id)
+//         test = test + `
+//         <tr>
+//             <td>${v.id}</td>
+//             <td>${v.title}</td>
+//             <td>
+//                 <img src='${v.url}' height='100px'/>
+//                 </td>
+//             <td>  <img src='${v.thumbnailUrl}' height='100px'/></td>
+//         </tr>
+        
+//         `
+//     })
+//     document.getElementById('showdata').innerHTML = test + `  </tbody></table>`;
+// })
+      // data.forEach(value => {
+          // console.log(value.id);
+
+          var shw = "";
+          const hi = document.getElementById('showdata');
+          
+          data.forEach((value)=>{
+
+              shw += `
+                    <tr>
+                    <td>${value.id}</td>
+                    <td>${value.title}</td>
+                    <td><img src='${value.url}' height='100px'/></td>
+                    </tr>
+              `
+          })
+              hi.innerHTML = shw;
+      });
+  
   return (
     <div className="App">
-      we can customize our homepage
+      <table border='1' id='showdata'></table>
     </div>
   );
 }
