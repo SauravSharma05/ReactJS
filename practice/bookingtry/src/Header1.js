77c50c69f2b2105420901ccd1eb60ee14454d85b
Saurav Sharma <saurav5122000@gmail.com>
2024-05-03 12:48:15 +0530

    prac updation

--- practice/bookingtry/src/Header1.js
+++ practice/bookingtry/src/Header1.js
@@ -5,8 +5,8 @@ function Header1() {
   return (
     <div className="App">
 
-        <div className='Header1 df'>
 
+        <div className='Header1 df jcsb w70'>
         <div className='Headera df aic'>Booking.com</div>
         <div className='Headerb df'>
             <div className='Headerb1 df aic'>India</div>
@@ -16,7 +16,20 @@ function Header1() {
             <div className='Headerb1 btn df aic'><button>Register</button></div>
             <div className='Headerb1 btn df aic'><button>Sign in</button></div>
         </div>
-            
+        </div>
+
+        <div className='Header1 df jcs w70'>
+            <div className='Headerb1 df aic'>Stays</div>
+            <div className='Headerb1 df aic'>Flights</div>
+            <div className='Headerb1 df aic'>Flight + Hotel</div>
+            <div className='Headerb1 df aic'>Car Rentals</div>
+            <div className='Headerb1 btn df aic'>Attractions</div>
+            <div className='Headerb1 btn df aic'>Airport taxis</div>
+        </div>
+
+        <div className='Header2 df jcs fdc w70'>
+            <div className='Headerb1 df aic'>Find your next stay</div>
+            <div className='Headerb1 df aic'>Search deals on hotels, homes and much more... </div>
         </div>
 
     </div>
