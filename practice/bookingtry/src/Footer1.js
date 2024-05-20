a6858a4869fa8f144458c56b3bd33738247969e5
Saurav Sharma <saurav5122000@gmail.com>
2024-05-20 21:42:11 +0530

    prac updation

--- practice/bookingtry/src/Footer1.js
+++ practice/bookingtry/src/Footer1.js
@@ -5,11 +5,11 @@ import React from "react";
     <div>
       <div className="Stay w70 df fdc aic">
         <div className="txt1">Stay in the know</div>
-        <div className="txt2">
+           <div className="txt2">
           Sign up to get marketing emails from Booking.com, including
           promotions, rewards, travel experiences, and information about
           Booking.com and Booking.com Transport Limited’s products and services.
-        </div>
+           </div>
         <div className="butsy txt1 df">
           <input type="email" placeholder="Your email address"></input>
           <button className="butsub butcol">Subscribe</button>
@@ -17,6 +17,7 @@ import React from "react";
         <div className="txt2">You can opt out anytime. See our <a href="#">privacy statement.</a>
         </div>
         </div>
+        
         <div className="w70 txt2 df footbar jcsb">
         <div className='Headerb1 df aic'>Mobile version</div>
             <div className='Headerb1 df aic'>Your account</div>
@@ -25,8 +26,46 @@ import React from "react";
             <div className='Headerb1 btn df aic'>Become an affiliate</div>
             <div className='Headerb1 btn df aic'>Booking.com for Business</div>
         </div>
+
+
         <div className="w70 df">
-            <div className='Headerb1 df aic'>
+            <div className='Headerb1 df fdc footl'>
+                  <div><li>Countries</li></div>
+                  <div><li>Regions</li></div>
+                  <div><li>Cities</li></div>
+                  <div><li>Districts</li></div>
+                  <div><li>Airports</li></div>
+                  <div><li>Hotels</li></div>
+                  <div><li>Places of interest</li></div>
+            </div>
+            <div className='Headerb1 df fdc footl'>
+                  <div><li>Countries</li></div>
+                  <div><li>Regions</li></div>
+                  <div><li>Cities</li></div>
+                  <div><li>Districts</li></div>
+                  <div><li>Airports</li></div>
+                  <div><li>Hotels</li></div>
+                  <div><li>Places of interest</li></div>
+            </div>
+            <div className='Headerb1 df fdc footl'>
+                  <div><li>Countries</li></div>
+                  <div><li>Regions</li></div>
+                  <div><li>Cities</li></div>
+                  <div><li>Districts</li></div>
+                  <div><li>Airports</li></div>
+                  <div><li>Hotels</li></div>
+                  <div><li>Places of interest</li></div>
+            </div>
+            <div className='Headerb1 df fdc footl'>
+                  <div><li>Countries</li></div>
+                  <div><li>Regions</li></div>
+                  <div><li>Cities</li></div>
+                  <div><li>Districts</li></div>
+                  <div><li>Airports</li></div>
+                  <div><li>Hotels</li></div>
+                  <div><li>Places of interest</li></div>
+            </div>
+            <div className='Headerb1 df fdc footl'>
                   <div><li>Countries</li></div>
                   <div><li>Regions</li></div>
                   <div><li>Cities</li></div>
@@ -36,7 +75,6 @@ import React from "react";
                   <div><li>Places of interest</li></div>
             </div>
             
-            <div className='Headerb1 df aic'>Your account</div>
         </div>
 
         
