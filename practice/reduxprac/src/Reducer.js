4d5e6f77643998f85b13b05d33e0f5055455a3df
Saurav Sharma <saurav5122000@gmail.com>
2024-06-01 05:43:29 +0530

    prac updation

---
+++ practice/reduxprac/src/Reducer.js
@@ -0,0 +1,11 @@
+
+export const reducerFun = (state=10,action)=>{
+    switch(action.type)
+    {
+     case 'increment' : return state+1
+ 
+     case 'decrement' : return state-1;
+ 
+     default : return state;
+    }
+ }
