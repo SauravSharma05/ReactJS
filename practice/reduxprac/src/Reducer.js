f3608109ce138610456ca33e4f75d73955a0ddc5
Saurav Sharma <saurav5122000@gmail.com>
2024-06-07 14:08:16 +0530

    prac updation

--- practice/reduxprac/src/Reducer.js
+++ practice/reduxprac/src/Reducer.js
@@ -1,11 +1,32 @@
 
-export const reducerFun = (state=10,action)=>{
-    switch(action.type)
-    {
-     case 'increment' : return state+1
+// export const reducerFun = (state=10,action)=>{
+//     switch(action.type)
+//     {
+//      case 'increment' : return state+1
  
-     case 'decrement' : return state-1;
+//      case 'decrement' : return state-1;
  
-     default : return state;
-    }
- }
+//      default : return state;
+//     }
+//  }
+
+
+ const userSlice = createSlice({
+    name: "users",
+    initialState: usersList,
+    reducers: {
+      addusers: (state, action) => {
+          // console.log(state)//[]
+          // console.log(action.payload);//{name: '', email: ''}
+  
+          state.push(action.payload)
+      },
+    },
+  });
+  
+   export const {addusers} = userSlice.actions;
+  //export const { increment, decrement, incrementByAmount } =
+  // counterSlice.actions
+  
+  export default userSlice.reducer;
+  // export default counterSlice.reducer
